4f2c877398b18ae44ae660e333f35673518210d4
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2025-01-24 18:08:44 -0800

    Change mpss.h signatures to add algorithm

--- mpss/mpss.h
+++ mpss/mpss.h
@@ -9,12 +9,18 @@
 #include <optional>
 
 namespace mpss {
+	enum class SignatureAlgorithm {
+		ECDSA_P256_SHA256,
+		ECDSA_P384_SHA384,
+		ECDSA_P521_SHA512
+	};
+
     /**
     * @brief Creates a new key pair with the given name.
     * @param name The name of the key pair.
     * @return True if the key pair was created successfully, false otherwise.
     */
-    bool create_key(std::string_view name);
+    bool create_key(std::string_view name, SignatureAlgorithm algorithm);
 
     /**
     * @brief Deletes the key pair with the given name.
@@ -26,30 +32,37 @@ namespace mpss {
     /**
     * @brief Signs the given data with the key pair with the given name.
     * @param name The name of the key pair.
-    * @param data The data to sign.
+    * @param data The hash to sign.
+	* @param algorithm The signature algorithm to use.
     * @return The signature if the data was signed successfully, an empty optional otherwise.
+	* @note The data needs to be hashed before signing. The hash algorithm should match the given signature algorithm.
     */
-    std::optional<std::string> sign(std::string_view name, std::string data);
+    std::optional<std::string> sign(std::string_view name, std::string_view hash, SignatureAlgorithm algorithm);
 
     /**
     * @brief Verifies the given data with the key pair with the given name.
     * @param name The name of the key pair.
-    * @param data The data to verify.
+    * @param hash The hash to verify.
 	* @param signature The signature to verify.
+	* @param algorithm The signature algorithm to use.
     * @return True if the data was verified successfully, false otherwise.
     */
-    bool verify(std::string_view name, std::string data, std::string signature);
+    bool verify(std::string_view name, std::string_view hash, std::string_view signature, SignatureAlgorithm algorithm);
 
     /**
-    * @brief Retrieves a verification-signing (public-private) key pair with the given name.
+    * @brief Retrieves a verification (public) key with the given name.
     * @param name The name of the key pair.
     * @param vk_out The verification key.
-    * @param sk_out The signing key.
-    * @return True if the key pair was retrieved successfully, false otherwise.
-    * @note This function should not be used unless there is a special need to retrieve pre-existing keys.
-    * Instead, the sign function should be used to sign data and the verify function should be used to verify signatures.
+    * @return True if the verification key retrieved successfully, false otherwise.
     */
-    bool get_key(std::string_view name, std::string& vk_out, std::string& sk_out);
+    bool get_key(std::string_view name, std::string& vk_out);
+
+    /**
+    * @brief Determines whether the given signature algorithm is supported in the safe storage system.
+    * @param algorithm The signature algorithm to verify
+	* @return True if the signature algorithm is supported in safe storage, false otherwise.
+    */
+	bool is_safe_storage_supported(SignatureAlgorithm algorithm);
 
     /**
     * @brief Retrieves the last error that occurred.
