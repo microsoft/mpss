14d3f560306f8b2c068deb8353d02ecc76429237
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2025-05-20 10:17:17 -0700

    Add comments for KeyInfo

--- mpss/key_info.h
+++ mpss/key_info.h
@@ -6,12 +6,22 @@
 #include <string>
 
 namespace mpss {
+    /**
+     * @brief Structure to hold information about a key.
+     */
     struct KeyInfo {
         KeyInfo(bool hardware_backed, const char *storage_description)
             : is_hardware_backed(hardware_backed), storage_description(storage_description)
         {}
 
+        /**
+         * @brief Indicates if the key is backed by hardware
+         */
         bool is_hardware_backed;
+
+        /**
+         * @brief Description of the storage where the key is stored.
+         */
         const char *storage_description;
     };
 } // namespace mpss
